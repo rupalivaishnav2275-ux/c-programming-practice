378329648255d7a5b4522ccec28ce08a2999f42e
RUP <rupalivaishnav2275@gmail.com>
2026-03-15 18:15:15 +0530

    Fixed Variable declaration.

---
+++ 02_variables.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main (){
+    int number =22;
+    char percentage = '%';
+    int age = 20;
+    age = 21;
+    float pi = 3.14;
+    
+    int a = 30;
+    int b = 80;
+
+    int _age = 20;
+    int initial_price = 50;
+    printf("number = %d\n", number);
+    printf("percentage = %c\n", percentage);
+    printf("age = %d\n", age);
+    printf("pi = %f\n", pi);
+    printf("a = %d\n", a);
+    printf("b = %d\n",b);
+    printf("_age = %d\n" , _age);
+    printf("initial price = %d\n" ,initial_price);
+
+    return 0;
+}
